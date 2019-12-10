545d2846848c646e100922ca3bd4596b99412d40
Sebastian Fuentes <sfuen@umich.edu>
2019-12-10 10:39:30 -0500

    shared memory working

--- new-forward.cuh
+++ new-forward.cuh
@@ -8,12 +8,12 @@ namespace mxnet
 namespace op
 {
 
-__constant__ float kernels[8192];
+__constant__ float kernels[16000];
 
 __global__ void forward_kernel(float *y, const float *x, const int B, const int M, const int C, const int H, const int W, const int K)
 {
 
-    __shared__ float inputImage[72*72];
+    __shared__ float inputImage[10000];
     /*
     The goal here is to be correct AND fast.
     We have some nice #defs for you below to simplify indexing. Feel free to use them, or create your own.
@@ -25,7 +25,7 @@ __global__ void forward_kernel(float *y, const float *x, const int B, const int 
 // y4d(0,0,0,0) = a
 #define y4d(i3, i2, i1, i0) y[(i3) * (M * H_out * W_out) + (i2) * (H_out * W_out) + (i1) * (W_out) + i0]
 #define x4d(i3, i2, i1) x[(i3) * (C * H * W) + (i2) * (H * W) + (i1)]
-#define sharedIndex(i1, i0) inputImage[(i1) * (W) + i0]
+#define sharedIndex(i1, i0) inputImage[(i1) * (W) + (i0)]
 #define k4d(i3, i2, i1, i0) kernels[(i3) * (C * K * K) + (i2) * (K * K) + (i1) * (K) + i0]
 
     const int H_out = H - K + 1;
@@ -48,8 +48,9 @@ __global__ void forward_kernel(float *y, const float *x, const int B, const int 
         }
         for (int channelNumber = 0; channelNumber < C; channelNumber++) {
             // read one channel into shared memory
-            for shared_index = threadIdx.x; shared_index < imageSize; shared_index += numThreads)
-                inputImage[shared_index] = sharedIndex(imageNumber, channelNumber, shared_index);
+            __syncthreads();
+            for (int shared_index = threadIdx.x; shared_index < imageSize; shared_index += numThreads)
+                inputImage[shared_index] = x4d(imageNumber, channelNumber, shared_index);
             __syncthreads();
             
             for (int index = threadIdx.x; index < outputSize; index += numThreads) {
